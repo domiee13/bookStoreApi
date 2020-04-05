596f98dce1272c136b68139e9f9cd82be56a53b2
Domiee <dungngocmd@gmail.com>
2020-04-05 14:05:52 +0700

    . . .

--- controllers/register.controller.js
+++ controllers/register.controller.js
@@ -1,7 +1,6 @@
 var User = require('../models/user.model');
 
 var bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
 
 module.exports.postRegister = function(req,res){
     const newUser = {
